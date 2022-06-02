76fa2577820d3c70dc72f45065bd3e06fe119bcb
Nicolas Derobert <derobert.pro@gmail.com>
2022-06-02 17:22:35 +0200

    removing of button 1.0.3

--- src/examples/App.js
+++ src/examples/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { CustomMessageModal } from "../lib";
 
-const App = () => (
-	<div>
-		Hello Text
-		<CustomMessageModal
-			buttonTextToDisplay={"Save"}
-			messageToDisplay={"Employee Created2!"}
-		></CustomMessageModal>
-	</div>
-);
-
+// const App = () =>
+function App(props) {
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+	
+	return (
+		<div>
+			Hello Text
+			<button onClick={handleOpen}>Hello</button>
+			<CustomMessageModal
+				isOpen={open}
+				onRequestClose={handleClose}
+				buttonTextToDisplay={"Save"}
+				messageToDisplay={"Employee Created2!"}
+			></CustomMessageModal>
+		</div>
+	);
+}
 export default App;
--- src/lib/components/CustomMessageModal.js
+++ src/lib/components/CustomMessageModal.js
@@ -1,44 +1,50 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: 400,
+	bgcolor: "background.paper",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
 };
 
 export default function CustomMessageModal(props) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => {
+		setOpen(false);
+		props.onRequestClose();
+	};
 
-  return (
-    <div>
-      <button onClick={handleOpen}>{props.buttonTextToDisplay}</button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>{props.messageToDisplay}
-          {/* <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography> */}
-        </Box>
-      </Modal>
-    </div>
-  );
+	const isOpen = props.isOpen;
+	// const closeRequest = props.onRequestClose;
+
+	React.useEffect(() => {
+		if (isOpen === true) {
+			handleOpen();
+		}
+		if (isOpen === false) {
+			handleClose();
+		}
+	}, [isOpen]);
+
+	return (
+		<div>
+			{/* <button onClick={handleOpen}>{props.buttonTextToDisplay}</button> */}
+			<Modal
+				open={open}
+				onClose={handleClose}
+				// aria-labelledby="modal-modal-title"
+				// aria-describedby="modal-modal-description"
+			>
+				<Box sx={style}>{props.messageToDisplay}</Box>
+			</Modal>
+		</div>
+	);
 }
